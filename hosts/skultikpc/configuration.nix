67fe0ee5796b7fa823b2a00f5250aa6da1d70024
Marley Reeves <marley@marlstar.com>
2026-01-28 22:23:22 +1000

    disable old ssh enabling code

--- hosts/skultikpc/configuration.nix
+++ hosts/skultikpc/configuration.nix
@@ -54,18 +54,6 @@
 	programs.firefox.enable = true;
 	programs.waybar.enable = true;
 
-
-	# Some programs need SUID wrappers, can be configured further or are
-	# started in user sessions.
-	# programs.mtr.enable = true;
-	programs.gnupg.agent = {
-		enable = true;
-		enableSSHSupport = true;
-	};
-
-	# Enable the OpenSSH daemon.
-	services.openssh.enable = true;
-
 	# Open ports in the firewall.
 	# networking.firewall.allowedTCPPorts = [ ... ];
 	# networking.firewall.allowedUDPPorts = [ ... ];
