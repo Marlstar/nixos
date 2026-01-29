4196f751e42b9e5fdcd76be7957990f4373dc24d
Marley Reeves <marley@marlstar.com>
2026-01-29 14:25:46 +1000

    gpg signing fix

--- modules/home/cli/gpg.nix
+++ modules/home/cli/gpg.nix
@@ -1,10 +1,11 @@
-{ ... }: {
+{ pkgs, ... }: {
 	programs.gpg = {
 		enable = true;
 	};
 
 	services.gpg-agent = {
 		enable = true;
+		pinentry.package = pkgs.pinentry-gtk2;
 
 		defaultCacheTtl = 86400;
 		maxCacheTtl = 86400;
