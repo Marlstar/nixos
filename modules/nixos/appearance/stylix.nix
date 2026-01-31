f893ae032f9cf36c7348bfaa824cab76426ffca5
Marley Reeves <marley@marlstar.com>
2026-01-31 22:57:08 +1000

    stylix: terminal font configs

--- modules/nixos/appearance/stylix.nix
+++ modules/nixos/appearance/stylix.nix
@@ -1,5 +1,14 @@
-{ inputs, ... }: {
+{ inputs, pkgs, ... }: {
 	stylix.enable = true;
 	stylix.polarity = "dark";
 	stylix.base16Scheme = "${inputs.tinted-schemes}/base16/catppuccin-mocha.yaml";
+	stylix.fonts = {
+		monospace = {
+			name = "JetBrains Mono Nerd Font";
+			package = pkgs.nerd-fonts.jetbrains-mono;
+		};
+	};
+	stylix.fonts.sizes = {
+		terminal = 14;
+	};
 }
