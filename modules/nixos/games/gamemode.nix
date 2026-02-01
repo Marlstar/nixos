a02f214d291863bf08b572d6332eed038518ab3a
Marley Reeves <marley@marlstar.com>
2026-02-01 14:05:23 +1000

    gamemode

---
+++ modules/nixos/games/gamemode.nix
@@ -0,0 +1,15 @@
+{ pkgs, ... }: {
+	programs.gamemode = {
+		enable = true;
+		settings = {
+			custom = {
+				start = "${pkgs.libnotify}/bin/notify-send 'GameMode started'";
+				end = "${pkgs.libnotify}/bin/notify-send 'GameMode ended'";
+			};
+		};
+	};
+
+	environment.systemPackages = with pkgs; [
+		libnotify
+	];
+}
