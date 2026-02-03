e5f202179038814fefefbc553deef2998e8d0d7e
Marley Reeves <marley@marlstar.com>
2026-02-03 15:20:04 +1000

    kanata

---
+++ modules/nixos/services/kanata/kanata.nix
@@ -0,0 +1,18 @@
+{ pkgs, ... }: {
+	environment.systemPackages = with pkgs; [
+		kanata
+	];
+
+	environment.etc."kanata/config.kbd".source = ./config.kbd;
+
+	systemd.services.kanata = {
+		description = "kanata";
+		wantedBy = ["multi-user.target"];
+		serviceConfig = {
+			Type = "simple";
+			ExecStart = "${pkgs.kanata}/bin/kanata -c /etc/kanata/config.kbd";
+			Restart = "on-failure";
+			SupplementaryGroups = [ "input" "uinput" ];
+		};
+	};
+}
