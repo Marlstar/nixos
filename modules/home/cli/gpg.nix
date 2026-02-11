3c9214efa9f03d0ca5816e3a42f6eef42aedb0c1
Marley Reeves <marley@marlstar.com>
2026-02-11 22:21:12 +1000

    gpg pinentry gtk2 -> gnome3

--- modules/home/cli/gpg.nix
+++ modules/home/cli/gpg.nix
@@ -5,11 +5,16 @@
 
 	services.gpg-agent = {
 		enable = true;
-		pinentry.package = pkgs.pinentry-gtk2;
+		# pinentry.package = pkgs.pinentry-gtk2;
+		pinentry.package = pkgs.pinentry-gnome3;
 
 		defaultCacheTtl = 86400;
 		maxCacheTtl = 86400;
 		defaultCacheTtlSsh = 86400;
 		maxCacheTtlSsh = 86400;
 	};
+
+	home.packages = with pkgs; [
+		gcr
+	];
 }
