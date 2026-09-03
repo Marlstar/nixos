3d6a8bd2150010c22d9c30a16448e5e0db43896e
Marley Reeves <marley@marlstar.com>
2026-09-03 14:27:19 +1000

    jupyter: add packages to kernel

--- modules/services/jupyterlab.nix
+++ modules/services/jupyterlab.nix
@@ -1,7 +1,30 @@
-{ ... }: {
+{ pkgs, ... }: let
+pythonEnv = pkgs.python3.withPackages (ps: with ps; [
+	ipykernel # required
+	numpy
+	pandas
+	matplotlib
+	scipy
+	scikit-learn
+]);
+
+logoPath = "${pythonEnv}/${pythonEnv.sitePackages}/ipykernel/resources";
+in {
 	services.jupyter = {
 		enable = true;
 		port = 9111;
 		password = "argon2:$argon2id$v=19$m=10240,t=10,p=8$SSeVfDayKtEl31+SFJ9wHA$1V+7H4bgDkC7F9b63yN+doP794GikCEuNsU/tNw0Nso";
+
+		kernels.python3 = {
+			displayName = "Python 3 [DS]";
+			language = "python";
+			argv = [
+				"${pythonEnv.interpreter}"
+				"-m" "ipykernel_launcher"
+				"-f" "{connection_file}"
+			];
+			logo32 = "${logoPath}/logo-32x32.png";
+			logo64 = "${logoPath}/logo-64x64.png";
+		};
 	};
 }
